08d651999f0f2f6a1008d2805a4ef62bfeb036e2
VladBel2000 <93825531+VladBel2000@users.noreply.github.com>
2022-02-27 18:39:43 +0300

    Add files via upload

---
+++ MyCalc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCalcLib
+{
+    public class MyCalc
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public int Del(int x, int y)
+        {
+            return x / y;
+        }
+        public int Min(int x, int y)
+        {
+            return x - y;
+        }
+        public int Pr(int x, int y)
+        {
+            return x * y;
+        }
+        public Boolean check_password(string password)
+        {
+            if (password.Length > 4)
+                return true;
+            else
+                return false;
+        }
+        public int max_list(List<int> list_digit)
+        {
+            int max = list_digit[0];
+            for (int i = 0; i < list_digit.Count; i++)
+            {
+                if (list_digit[i] > max)
+                    max = list_digit[i];
+            }
+            return max;
+        }
+        public int Min(int a, int b, int c)
+        {
+            int min = a;
+            if (min > b) min = b;
+            if (min > c) min = c;
+            return min;
+        }
+
+        public int hour_in_min(int a)
+        {
+            return a * 60;
+        }
+    }
+}
---
+++ MyCalcTests.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalcLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalcLib.Tests
+{
+    [TestClass()]
+    public class MyCalcTests
+    {
+        [TestMethod()]
+        public void Sum_10_20_Return_30()
+        {
+            // arrange
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.Sum(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void Min_50_45()
+        {
+            // arrange
+            int x = 50;
+            int y = 45;
+            int expected = 5;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.Min(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Pr_3_4()
+        {
+            // arrange
+            int x = 3;
+            int y = 4;
+            int expected = 12;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.Pr(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Del_20_5()
+        {
+            // arrange
+            int x = 20;
+            int y = 5;
+            int expected = 4;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.Del(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Del_20_0()
+        {
+            // arrange
+            int x = 20;
+            int y = 0;
+            int expected = 0;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.Del(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Del_0_20()
+        {
+            // arrange
+            int x = 0;
+            int y = 20;
+            int expected = 0;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.Del(x, y);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void test_check_password()
+        {
+            // arrange
+            string password = "Hello-81";
+            Boolean expected = true;
+
+            // act
+            MyCalc help = new MyCalc();
+            Boolean actual = help.check_password(password);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void test_max_list()
+        {
+            // arrange
+            List<int> list_digit = new List<int>();
+            list_digit.Add(5);
+            list_digit.Add(-4);
+            list_digit.Add(19);
+            list_digit.Add(-89);
+
+            int y = 0;
+            int expected = 19;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.max_list(list_digit);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void TestMin()
+        {
+            // arrange
+            int expected = 3;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.Min(3, 4, 5);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void TestHourInMin()
+        {
+            // arrange
+            int expected = 180;
+
+            // act
+            MyCalc help = new MyCalc();
+            int actual = help.hour_in_min(3);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
