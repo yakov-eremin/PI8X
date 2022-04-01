03f6726aaa2ef4d62b0f861a2404263054b00ea1
Rukin Danil <rukin2018d@yandex.ru>
2022-04-01 16:20:57 +0700

    Проверка двух одинаковых вариантов

---
+++ QA_Lab4_RukinDanil/PasswordManager/PasswordManager.Tests/RockPaperScissorsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordManager.DAL.Entities;
+using PasswordManager.DAL.Repositories;
+using PasswordManager.DAL.Repositories.Interfaces;
+using PasswordManager.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Tests
+{
+    [TestClass]
+    public class RockPaperScissorsTests
+    {
+        [TestMethod]
+        public void GetWinningVariant_EqualVariants_ReturnReceivedVariant()
+        {
+            // arrange
+            RockPaperScissors game = new RockPaperScissors();
+            ChoiceVariant firstVariant = ChoiceVariant.Stone;
+            ChoiceVariant secondVariant = ChoiceVariant.Stone;
+
+            // act
+            ChoiceVariant result = game.GetWinningVariant(firstVariant, secondVariant);
+
+            // assert
+
+            Assert.AreEqual(firstVariant, result);
+            Assert.AreEqual(secondVariant, result);
+        }
+    }
+}
--- QA_Lab4_RukinDanil/PasswordManager/PasswordManager.UseCases/RockPaperScissors.cs
+++ QA_Lab4_RukinDanil/PasswordManager/PasswordManager.UseCases/RockPaperScissors.cs
@@ -7,11 +7,41 @@ using System.Threading.Tasks;
 
 namespace PasswordManager.UseCases
 {
+    /// <summary>
+    /// Игра 'Камень ножницы бумага'
+    /// </summary>
+    /// <inheritdoc/>
     public class RockPaperScissors : Game
     {
         public override Guid GetAccessToken()
         {
             throw new NotImplementedException();
         }
+
+        public ChoiceVariant GetWinningVariant(ChoiceVariant firstVariant, ChoiceVariant secondVariant)
+        {
+            if (firstVariant == secondVariant)
+                return firstVariant;
+            return ChoiceVariant.Paper;
+        }
+    }
+
+    /// <summary>
+    /// Вариант выбора в игре 'Камень ножницы бумага'
+    /// </summary>
+    public enum ChoiceVariant
+    {
+        /// <summary>
+        /// Камень
+        /// </summary>
+        Stone,
+        /// <summary>
+        /// Ножницы
+        /// </summary>
+        Scissors,
+        /// <summary>
+        /// Бумага
+        /// </summary>
+        Paper
     }
 }
