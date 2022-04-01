1e479fb15576239218d1b02049e9d26c18766bc6
Rukin Danil <rukin2018d@yandex.ru>
2022-04-01 18:07:49 +0700

    Пофиксил установку количества раундов

--- QA_Lab4_RukinDanil/PasswordManager/PasswordManager.UseCases/RockPaperScissors.cs
+++ QA_Lab4_RukinDanil/PasswordManager/PasswordManager.UseCases/RockPaperScissors.cs
@@ -13,19 +13,33 @@ namespace PasswordManager.UseCases
     /// <inheritdoc/>
     public abstract class RockPaperScissors : Game
     {
+        /// <summary>
+        /// Матрица вариантов
+        /// </summary>
         protected ChoiceVariant[,] _matrix = new ChoiceVariant[,]
             {
                 { ChoiceVariant.Stone, ChoiceVariant.Stone, ChoiceVariant.Paper},
                 { ChoiceVariant.Stone, ChoiceVariant.Scissors, ChoiceVariant.Scissors},
                 { ChoiceVariant.Paper, ChoiceVariant.Scissors, ChoiceVariant.Paper }
             };
-
+        /// <summary>
+        /// Выбор пользователя
+        /// </summary>
         public ChoiceVariant UserChoice { get; set; }
         protected int _roundsCount;
+        /// <summary>
+        /// Количество раундов, не может быть четным, нулевым и отрицательным
+        /// </summary>
         public int RoundsCount
         {
             get => _roundsCount;
-            set => _roundsCount = (value / 2 == 0 ? value + 1 : value);
+            set
+            {
+                if (value < 0)
+                    _roundsCount = 1;
+                else
+                    _roundsCount = (value / 2 == 0 ? value + 1 : value);
+            }
         }
 
         public RockPaperScissors()
@@ -33,7 +47,12 @@ namespace PasswordManager.UseCases
             UserChoice = ChoiceVariant.Paper;
             RoundsCount = 1;
         }
-
+        /// <summary>
+        /// Проверяет два варианта и выбирает 'сильнейший'
+        /// </summary>
+        /// <param name="firstVariant"></param>
+        /// <param name="secondVariant"></param>
+        /// <returns></returns>
         public ChoiceVariant GetWinningVariant(ChoiceVariant firstVariant, ChoiceVariant secondVariant)
         {
             return _matrix[(int)firstVariant, (int)secondVariant];
