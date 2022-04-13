4865cbbf49ece3ec6a5e8047da745db11bf21c62
Tronko-Artem <undfprod@gmail.com>
2022-04-14 00:27:31 +0700

    Added GameField and GameField.draw_board().

--- QA_4_TicTac/pythonProject1/main.py
+++ QA_4_TicTac/pythonProject1/main.py
@@ -1,5 +1,4 @@
-
-  #Класс игрового поля
+#Класс игрового поля
 class GameField(object):
    def __init__(self, board):
        self.board = board
@@ -10,18 +9,17 @@ class GameField(object):
            print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
            print("-------------")
 
-#
-#    def check_win(board):
-#        win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-#        for each in win_coord:
-#            if board[each[0]] == board[each[1]] == board[each[2]]:
-#                return board[each[0]]
-#        return False
+   # def check_win(board):
+   #     win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+   #     for each in win_coord:
+   #         if board[each[0]] == board[each[1]] == board[each[2]]:
+   #             return board[each[0]]
+   #     return False
 
-# #Класс ИИ (в нашем случае, искусственного идиота), соревнующегося с игроком
-# class VirtualPlayer(object):
-#     def __init__(self, mark):
-#         self.mark = mark
+#Класс ИИ (в нашем случае, искусственного идиота), соревнующегося с игроком
+class VirtualPlayer(object):
+    def __init__(self, mark):
+        self.mark = mark
 
 
 # Press the green button in the gutter to run the script.
@@ -32,4 +30,6 @@ if __name__ == '__main__':
     #Создадим игровое поле
     Game = GameField(board)
     Game.draw_board(board)
+
+
 
