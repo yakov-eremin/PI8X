b83764cf7d9a7b3eb04c17cf71968abe730fe9a6
sablist99@bk.ru <sablist99@bk.ru>
2022-04-19 00:42:05 +0700

    Загружен проект по 4 лабораторной. Тема "Программа для напоминания о днях рождения друзей"

---
+++ Lab 4/Lab 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Lab_4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в напоминалку");
+        }
+
+        [Fact]
+        public void Test()
+        {
+            Console.WriteLine("Это пустой тест");
+        }
+    }
+}
