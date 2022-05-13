0f3d3273960b3894aae4cb42951381b091e87ee9
MrsDramaqueen <chelyaev_99@mail.ru>
2022-05-13 22:16:47 +0700

    added test for right input

--- Chelyaev/QA_Lab_4_Chelyaev/tests.py
+++ Chelyaev/QA_Lab_4_Chelyaev/tests.py
@@ -12,5 +12,11 @@ class MyTests(unittest.TestCase):
     def choice_Sign_Test(self):
         self.assertIn(main.player_sign_choice(), "xo")
 
+    #Тест на правильность введенных данных
+    def right_input_test(self):
+        self.assertIs(main.input_sign(player_choice='X'), True)
+        self.assertIs(main.input_sign(player_choice='O'), True)
+
+
 if __name__ == '__main__':
     unittest.main()
