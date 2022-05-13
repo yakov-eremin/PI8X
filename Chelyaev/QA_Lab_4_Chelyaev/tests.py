6e5b0a821980f8dc76bc64fba354ded8aaf710ed
MrsDramaqueen <chelyaev_99@mail.ru>
2022-05-13 21:27:44 +0700

    added test for choice sign

--- Chelyaev/QA_Lab_4_Chelyaev/tests.py
+++ Chelyaev/QA_Lab_4_Chelyaev/tests.py
@@ -8,5 +8,9 @@ class MyTests(unittest.TestCase):
         main.GameField.draw_field(self, field=range(1,10))
         self.assertIsNotNone(sys.stdout)
 
+    #Тест на проверку правильности выбора знака пользователем
+    def choice_Sign_Test(self):
+        self.assertIn(main.player_sign_choice(), "xo")
+
 if __name__ == '__main__':
     unittest.main()
