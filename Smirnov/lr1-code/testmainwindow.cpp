069b248d060926d032101deeee32c3a9705d7300
Roman <roman.smi2000@yandex.ru>
2022-05-13 16:09:57 +0300

    Ten data-based UNIT-Tests fot MainWindow::getBrightness() with wrong tests

--- Smirnov/lr1-code/testmainwindow.cpp
+++ Smirnov/lr1-code/testmainwindow.cpp
@@ -10,14 +10,14 @@ void TestMainWindow::getBrightness_data() {
     QTest::addColumn<int>("brightness");
 
     QTest::newRow("1") << 0xfff3e5ab << 226;
-    QTest::newRow("2") << 0xff27261a << 36;
+    QTest::newRow("2") << 0xff27261a << 360;
     QTest::newRow("3") << 0xffedff21 << 224;
     QTest::newRow("4") << 0xff434750 << 70;
     QTest::newRow("5") << 0xff423c63 << 66;
-    QTest::newRow("6") << 0xff35682d << 82;
+    QTest::newRow("6") << 0xff35682d << 812;
     QTest::newRow("7") << 0xffb27070 << 131;
     QTest::newRow("8") << 0xff20b2aa << 133;
-    QTest::newRow("9") << 0xfffadadd << 228;
+    QTest::newRow("9") << 0xfffadadd << 128;
     QTest::newRow("10") << 0xfffde910 << 214;
 }
 
