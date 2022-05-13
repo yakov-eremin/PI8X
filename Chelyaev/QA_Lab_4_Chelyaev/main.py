2671aa0d20dc676a54c3feae37e6f1a7797c7407
MrsDramaqueen <chelyaev_99@mail.ru>
2022-05-14 01:18:33 +0700

    added choice sector for previos test

--- Chelyaev/QA_Lab_4_Chelyaev/main.py
+++ Chelyaev/QA_Lab_4_Chelyaev/main.py
@@ -1,3 +1,22 @@
+field = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#Установка символа в выбранную секцию
+def choice_sector(player_choice):
+    done = False
+    while not done:
+        print("В какой сектор вы хотите поставить ваш знак?")
+        player_answer = input()
+        player_answer = int(player_answer)
+        if player_answer >= 1 and player_answer <= 9:
+            if (str(field[player_answer - 1]) not in "XO"):
+                field[player_answer - 1] = player_choice
+                done = True
+            else:
+                print("ВЫ не можете занять этот сектор. Он уже занят")
+        else:
+            print("Введите число от 1 до 9, в зависимости от того, какой сектор хотите занять.")
+    return done
+
 #Выбор знака
 def choice_sign():
     print("За кого будете играть? (х или о)")
