9cbc994a148904dbb2f512aefcdef2d3d5f4d864
Roman <roman.smi2000@yandex.ru>
2022-05-13 15:35:34 +0300

    Five simple UNIT-Tests fot MainWindow::getBrightness() with wrong test

--- Smirnov/lr1-code/testmainwindow.cpp
+++ Smirnov/lr1-code/testmainwindow.cpp
@@ -9,7 +9,7 @@ void TestMainWindow::getBrightness() {
     MainWindow w;
 
     QCOMPARE(w.getBrightness(0xfff3e5ab),226);
-    QCOMPARE(w.getBrightness(0xff27261a),36);
+    QCOMPARE(w.getBrightness(0xff27261a),366);
     QCOMPARE(w.getBrightness(0xffedff21),224);
     QCOMPARE(w.getBrightness(0xff434750),70);
     QCOMPARE(w.getBrightness(0xff423c63),66);
