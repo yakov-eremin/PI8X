3f56aa4e6fbef69b8c2a837e7a1cf420d9e2562a
Roman <roman.smi2000@yandex.ru>
2022-05-14 17:10:48 +0300

    Smirnov-Lab4-direction()-Code7

--- Smirnov/15Puzzle/game.cpp
+++ Smirnov/15Puzzle/game.cpp
@@ -15,8 +15,8 @@ uint Game::move() {
 
 uint Game::direction(uint x, uint y) {
     if (x < 3 && !grid.itemAtPosition(x+1,y)) return 3;
-    if (y > 0 && !grid.itemAtPosition(x,y-1)) return 2;
-    if (x > 0 && !grid.itemAtPosition(x-1,y)) return 1;
+    if (y > 0 && y < 4 && !grid.itemAtPosition(x,y-1)) return 2;
+    if (x > 0 && x < 4 && !grid.itemAtPosition(x-1,y)) return 1;
     if (y < 3 && !grid.itemAtPosition(x,y+1)) return 4;
     return 0;
 }
