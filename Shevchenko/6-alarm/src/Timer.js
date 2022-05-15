d2cd8a97f13f122fd954894d2500352e3a9c6fab
widrelk <widrelk@gmail.com>
2022-05-15 14:16:09 +0700

    Update Timer realization

--- Shevchenko/6-alarm/src/Timer.js
+++ Shevchenko/6-alarm/src/Timer.js
@@ -1,11 +1,39 @@
 class Timer {
-	constructor(name, delay, soundIndex, callback) {
-		this.name = name;
-		this.delay = delay;
-		this.soundIndex = soundIndex;
-		this.callBack = callback;
+	#name;
+	#soundIndex;
+	#triggered;
+	#hours;
+	#minutes;
+	#seconds;
 
-		setTimeout(() => callback(this), delay);
+	constructor(name, soundIndex, callback, h, m, s) {
+		this.#name = name;
+		this.#soundIndex = soundIndex;
+		this.#triggered = false;
+		this.#hours = h;
+		this.#minutes = m;
+		this.#seconds = s;
+
+		const currDate = new Date()
+
+		const targetDate = new Date();
+		targetDate.setHours(h, m, s);
+
+		setTimeout(() => {
+			callback();
+			this.#triggered = true;
+			}, Math.abs(currDate - targetDate));
+	}
+
+	getDisplayInfo() {
+		return({
+			name: this.#name,
+			triggered: this.#triggered,
+			soundIndex: this.#soundIndex,
+			h: this.#hours,
+			m: this.#minutes,
+			s: this.#seconds,
+		})
 	}
 }
 
