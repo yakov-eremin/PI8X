4fa5f220d860e775d607e82aecb2961e29737902
bytilda <52560053+bytilda@users.noreply.github.com>
2022-05-16 05:52:13 +0700

    Написана основная программа.

--- Pesnyak/Лабораторная работа №4/src/main/java/com/example/bdd/BddApplication.java
+++ Pesnyak/Лабораторная работа №4/src/main/java/com/example/bdd/BddApplication.java
@@ -1,9 +1,30 @@
 package com.example.bdd;
 
+import java.util.Scanner;
+
 public class BddApplication {
 
     public static void main(String[] args) {
+        Therapist therapist = new Therapist();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ваш симптом: ");
+        String symptom = scanner.nextLine();
+        therapist.tellTherapistAboutSymptom(symptom);
 
+        System.out.print("Ваш второй симптом: ");
+        symptom = scanner.nextLine();
+        therapist.tellTherapistAboutSymptom(symptom);
+        Answer answer;
+        while(true){
+            answer = therapist.ask();
+            System.out.println(answer.getAnswer());
+            if(answer.isDiagnosis)
+                break;
+            else{
+                String myAnswer = scanner.nextLine();
+                therapist.answerToTherapist(myAnswer);
+            }
+        }
     }
 
 }
