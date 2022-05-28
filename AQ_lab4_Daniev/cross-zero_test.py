c3c29a308af9fb188f15d666b475b54e6913e3b4
NPC246 <45330737+NPC246@users.noreply.github.com>
2022-05-28 22:59:19 +0700

    Добавил тест на выбор ячейки поля

--- AQ_lab4_Daniev/cross-zero_test.py
+++ AQ_lab4_Daniev/cross-zero_test.py
@@ -2,13 +2,19 @@ import sys
 import unittest
 import main
 
-
 class GameTests(unittest.TestCase):
 
     # Проверка, заполняется ли игровое поле
     def test_Create_Cross_Zero_Fieldd(self):
         main.CrossZeroField.draw_game_board(self, gameBoard=range(1, 10))
         self.assertIsNotNone(sys.stdout)
+
+    def test_Correct_Sector_Choice(self):
+        self.assertIs(main.put_sign(player_choice="X"), True)
+        self.assertIs(main.put_sign(player_choice="O"), True)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
