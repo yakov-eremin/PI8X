8a8c0cb18069196c4ea7108503a09d21d0acea48
NPC246 <45330737+NPC246@users.noreply.github.com>
2022-05-28 23:03:30 +0700

    Добал выбор ячейки для выставление знаков

--- AQ_lab4_Daniev/main.py
+++ AQ_lab4_Daniev/main.py
@@ -11,9 +11,28 @@ class CrossZeroField(object):
             print("|", " - ", "|", " - ", "|", " - ", "|")
             print ("-" * 19)
 
+def put_sign(player_choice):
+    valid = False
+    while not valid:
+        player_decision = input("Куда поставим " + player_choice+"? ")
+        try:
+            player_decision = int(player_decision)
+        except:
+            print ("Некорректный ввод")
+            continue
+        if player_decision >= 1 and player_decision <= 9:
+            if (str(gameBoard[player_decision-1]) not in "XO"):
+                gameBoard[player_decision-1] = player_choice
+                valid = True
+            else:
+                print("Клетка уже занята")
+        else:
+            print("Введите число от 1 до 9")
+    return valid
+
 if __name__ == '__main__':
     
     Game = CrossZeroField(gameBoard)
     Game.draw_game_board(gameBoard)
 
-    
+
