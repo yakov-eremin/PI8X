0a9b48fb7e82d704d5f85a6f3dd822c42a1e2ac5
NPC246 <45330737+NPC246@users.noreply.github.com>
2022-05-28 23:16:52 +0700

    добавление теста на достижение условий победы

--- AQ_lab4_Daniev/cross-zero_test.py
+++ AQ_lab4_Daniev/cross-zero_test.py
@@ -4,16 +4,19 @@ import main
 
 class GameTests(unittest.TestCase):
 
-    # Проверка, заполняется ли игровое поле
+    # Тест на заполние игрового поля
     def test_Create_Cross_Zero_Fieldd(self):
         main.CrossZeroField.draw_game_board(self, gameBoard=range(1, 10))
         self.assertIsNotNone(sys.stdout)
 
+    # Тест на правильный выбор ячейки для символа
     def test_Correct_Sector_Choice(self):
         self.assertIs(main.put_sign(player_choice="X"), True)
         self.assertIs(main.put_sign(player_choice="O"), True)
 
-
+    #Тест на достижение условий победы
+    def test_win_condition_success(self):
+        self.assertIsNot(main.CrossZeroField.win_condition_success(self, gameBoard=range(1,10)), True)
 
 
 if __name__ == '__main__':
